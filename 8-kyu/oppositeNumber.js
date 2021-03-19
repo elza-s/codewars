418d64fefa6de7c8fabc1c3b77c0945984cd29ec
elza-s <elza_somu@yahoo.com>
2021-03-19 09:50:06 -0400

    oppo numbers

---
+++ 8-kyu/oppositeNumber.js
@@ -0,0 +1,18 @@
+// Very simple, given a number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+
+function opposite(number) {
+  //your code here
+  if (Math.sign(number)===1){
+    return -Math.abs(number)
+      } else{
+
+      return Math.abs(number)
+      }
+}
